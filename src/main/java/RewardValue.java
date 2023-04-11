1cf1ef879c588ebcb7184df68fe1ddabf09f9696
hehehe123-hehe <15660012@student.uin-suka.ac.id>
2023-04-11 21:24:45 +0700

    hehehe

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,23 @@
+public class RewardValue {
+    private final double cashValue;
+    private final int milesValue;
+    public static final double CONVERSION_RATE = 0.0035;
+    // Constructor that accepts a cash value and calculates the miles value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) ((int) cashValue / CONVERSION_RATE);
+    }
+    // Constructor that accepts a miles value and calculates the cash value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * CONVERSION_RATE;
+    }
+    // Method to get the cash value of the RewardValue
+    public double getCashValue() {
+        return cashValue;
+    }
+    // Method to get the miles value of the RewardValue
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
